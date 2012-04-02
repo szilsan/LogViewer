6fd2758b15624619aae5a57de32bf804645dc269
szilsan <szilsan@gmail.com>
2012-04-02 09:19:45 +0200

    minor - menu

--- src/com/ftl/logview/gui/menu/FileMenu.scala
+++ src/com/ftl/logview/gui/menu/FileMenu.scala
@@ -23,64 +23,62 @@ object FileMenu extends Menu("File") {
       }
     }
     tooltip = "Open new log file in a new tab"
+  }
 
-    contents += new MenuItem("Open style file") {
-      reactions += {
-        case ButtonClicked(b) => {
-          var fc = new FileChooser
-          fc.showOpenDialog(this)
-          fc.fileSelectionMode = FileChooser.SelectionMode.FilesOnly
-          fc.multiSelectionEnabled = false
-          val file = fc.selectedFile
-          if (file != null) {
-            val bundle = LogViewMainFrame.tabAndBundle.get(LogViewMainFrame.tabbedPane.selection.page)
-            if (bundle.isDefined) {
-              Dialog.showMessage(null, "Not implemented", "Info")
-            }
+  contents += new MenuItem("Open style file") {
+    reactions += {
+      case ButtonClicked(b) => {
+        var fc = new FileChooser
+        fc.showOpenDialog(this)
+        fc.fileSelectionMode = FileChooser.SelectionMode.FilesOnly
+        fc.multiSelectionEnabled = false
+        val file = fc.selectedFile
+        if (file != null) {
+          val bundle = LogViewMainFrame.tabAndBundle.get(LogViewMainFrame.tabbedPane.selection.page)
+          if (bundle.isDefined) {
+            Dialog.showMessage(null, "Not implemented", "Info")
           }
         }
       }
-      tooltip = "Open style file and use on selected tab"
     }
+    tooltip = "Open style file and use on selected tab"
+  }
 
-    contents += new Separator
+  contents += new Separator
 
-    contents += new MenuItem("Close tab") {
-      reactions += {
-        case ButtonClicked(b) => {
-          if (LogViewMainFrame.tabAndBundle.nonEmpty) {
-            var page = LogViewMainFrame.tabbedPane.selection
-            LogViewMainFrame.tabAndBundle.remove(page.page)
-            LogViewMainFrame.tabbedPane.pages.remove(page.index)
-          } else {
-            Dialog.showMessage(null, "There is no tab opened", "Warning", Dialog.Message.Warning)
-          }
-        }
-      }
-      tooltip = "Close selected tab"
-    }
-
-    contents += new MenuItem("Close all tab") {
-      reactions += {
-        case ButtonClicked(b) => {
-          LogViewMainFrame.tabAndBundle.empty
-          LogViewMainFrame.tabbedPane.pages.remove(0, LogViewMainFrame.tabbedPane.pages.length)
-        }
-      }
-      tooltip = "Close all tabs"
-    }
-
-    contents += new Separator
-
-    contents += new MenuItem("Exit") {
-      reactions += {
-        case ButtonClicked(b) => {
-          exit(0)
+  contents += new MenuItem("Close tab") {
+    reactions += {
+      case ButtonClicked(b) => {
+        if (LogViewMainFrame.tabAndBundle.nonEmpty) {
+          var page = LogViewMainFrame.tabbedPane.selection
+          LogViewMainFrame.tabAndBundle.remove(page.page)
+          LogViewMainFrame.tabbedPane.pages.remove(page.index)
+        } else {
+          Dialog.showMessage(null, "There is no tab opened", "Warning", Dialog.Message.Warning)
         }
       }
     }
+    tooltip = "Close selected tab"
+  }
 
+  contents += new MenuItem("Close all tab") {
+    reactions += {
+      case ButtonClicked(b) => {
+        LogViewMainFrame.tabAndBundle.empty
+        LogViewMainFrame.tabbedPane.pages.remove(0, LogViewMainFrame.tabbedPane.pages.length)
+      }
+    }
+    tooltip = "Close all tabs"
+  }
+
+  contents += new Separator
+
+  contents += new MenuItem("Exit") {
+    reactions += {
+      case ButtonClicked(b) => {
+        exit(0)
+      }
+    }
     tooltip = "Exit"
-
   }
 }
