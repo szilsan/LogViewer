219ce4b2c35f4a57981b27a6eb3fb92d23a88c01
szilsan <szilsan@gmail.com>
2012-04-03 15:48:27 +0200

    Color chooser added

--- src/com/ftl/logview/gui/StyleInputPanel.scala
+++ src/com/ftl/logview/gui/StyleInputPanel.scala
@@ -1,28 +1,93 @@
 package com.ftl.logview.gui
-import scala.swing.Dialog
+import java.awt.Color
 import java.awt.Dimension
-import scala.swing.TextField
-import scala.swing.Button
-import scala.swing.Panel
+import scala.swing.event.ButtonClicked
 import scala.swing.BorderPanel
+import scala.swing.Button
+import scala.swing.Dialog
+import scala.swing.FlowPanel
+import scala.swing.Label
+import scala.swing.TextField
+import com.ftl.logview.LogViewBundle
+import javax.swing.JColorChooser
+import com.ftl.logview.logic.StyleUtil
 
 /**
  * Style input panel - get style expression and Color
  */
-class StyleInputPanel extends Dialog {
-  preferredSize = new Dimension(320, 200)
+class StyleInputPanel(bundle: LogViewBundle) extends Dialog {
+  preferredSize = new Dimension(650, 140)
   modal = true
 
-  var expression: TextField = new TextField
+  title = "Style input"
 
-  var okBtn: Button = new Button("OK")
-  var cancelBtn: Button = new Button("Cancel")
+  // expression definition
+  var expression = new TextField(50)
 
+  // color choosers
+  var expColorBg = Color.WHITE
+  var expColorFg = Color.BLACK
+
+  var sampleText = new Label("Sample text")
+  sampleText.foreground = expColorFg
+  sampleText.background = expColorBg
+  sampleText.opaque = true
   
-  
-  contents = new BorderPanel {
-	  add(expression, BorderPanel.Position.North)
+
+  var colorBtnBg = new Button(" Background color ") {
+    reactions += {
+      case ButtonClicked(b) =>
+        expColorBg = JColorChooser.showDialog(null, "Background color", expColorBg)
+        sampleText.background = expColorBg
+    }
   }
- 
+
+  var colorBtnFg = new Button(" Foreground color ") {
+    reactions += {
+      case ButtonClicked(b) =>
+        expColorFg = JColorChooser.showDialog(null, "Foreground color", expColorFg)
+        sampleText.foreground = expColorFg
+    }
+  }
+
+  // ok button
+  var okBtn: Button = new Button("OK") {
+    reactions += {
+      case ButtonClicked(b) =>
+        StyleUtil.addStyle(bundle.sc, expression.text, expColorFg, expColorBg)
+        bundle.styles += (expression.text -> expression.text)
+        bundle.logViewFrame.reloadData
+        StyleInputPanel.this.close()
+    }
+  }
+
+  // cancel button
+  var cancelBtn: Button = new Button("Cancel") {
+    reactions += {
+      case ButtonClicked(b) => StyleInputPanel.this.close()
+    }
+  }
+
+  contents = new BorderPanel {
+
+    add(new BorderPanel {
+      add(new FlowPanel {
+        contents += new Label("Expression: ")
+        contents += expression
+      }, BorderPanel.Position.North)
+      add(new FlowPanel {
+        contents += colorBtnBg
+        contents += colorBtnFg
+        contents += sampleText
+      }, BorderPanel.Position.Center)
+    }, BorderPanel.Position.North)
+
+    add(new FlowPanel {
+      contents += okBtn
+      contents += cancelBtn
+    }, BorderPanel.Position.Center)
+  }
+
   visible = true
+
 }
--- src/com/ftl/logview/gui/menu/HighlightMenu.scala
+++ src/com/ftl/logview/gui/menu/HighlightMenu.scala
@@ -5,12 +5,19 @@ import scala.swing.Menu
 import scala.swing.MenuItem
 import com.ftl.logview.gui.LogViewMainFrame
 import com.ftl.logview.gui.StyleInputPanel
+import com.ftl.logview.LogViewBundle
+import com.ftl.logview.LogViewBundle
 
 object HighlightMenu extends Menu("HighLight") {
   contents += new MenuItem("Highlighted expressions") {
     reactions += {
       case ButtonClicked(b) =>
-        doOnMenuSelect({new StyleInputPanel()})
+        doOnMenuSelect(
+          (bundle: LogViewBundle) =>
+            {
+              require(bundle != null)
+              new StyleInputPanel(bundle)
+            })
     }
   }
 
@@ -22,26 +29,27 @@ object HighlightMenu extends Menu("HighLight") {
     }
   }
 
-  private def doOnSkipExpression: Unit = {
+  private def doOnSkipExpression(bundle: LogViewBundle) {
+
+    require(bundle != null)
+
     val skipExpression = Dialog.showInput[String](null, "Skip text expression", "Skipped text", Dialog.Message.Question, null, Seq.empty, null)
     skipExpression match {
       case None => None
       case Some(exp) =>
-        var page = LogViewMainFrame.tabbedPane.selection
-        val bundle = LogViewMainFrame.tabAndBundle.get(page.page)
-        bundle match {
-          case None => None
-          case Some(s) =>
-            s.skippedList += exp
-            s.logViewFrame.reloadData
-        }
+        bundle.skippedList += exp
+        bundle.logViewFrame.reloadData
     }
   }
 
-  def doOnMenuSelect(doFunc: => Unit) {
-    println(LogViewMainFrame.tabAndBundle.size)
+  def doOnMenuSelect(doFunc: LogViewBundle => Unit) {
     if (LogViewMainFrame.tabAndBundle.nonEmpty) {
-      doFunc
+      var page = LogViewMainFrame.tabbedPane.selection
+      val bundle = LogViewMainFrame.tabAndBundle.get(page.page)
+      bundle match {
+        case None => None
+        case Some(b) => doFunc(b)
+      }
     } else {
       Dialog.showMessage(null, "There is no tab opened", "Warning", Dialog.Message.Warning)
     }
